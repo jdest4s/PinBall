403d10218edf8a4dd870dd72f3ca360602f9bc21
jdest4s <jdest4s@yahoo.co.jp>
2021-04-22 22:26:03 +0900

    Initial commit

---
+++ Assets/FripperTouch.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperTouch : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int touchCount = Input.touchCount;
+
+        for (int i = 0; i < touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began: // 画面に指が触れた時に行いたい処理をここに書く
+
+                    //画面の左側をタップしたら左のフリッパーが動く
+                    if ((touch.position.x <= Screen.width / 2) && (touch.phase == TouchPhase.Began && tag == "LeftFripperTag"))
+                    {
+                        SetAngle(this.flickAngle);
+                    }
+
+                    //画面の右側をタップしたら右のフリッパーが動く
+                    if ((touch.position.x >= Screen.width / 2) && (touch.phase == TouchPhase.Began && tag == "RightFripperTag"))
+                    {
+                        SetAngle(this.flickAngle);
+                    }
+                    break;
+
+                case TouchPhase.Ended: // 画面から指が離れた時に行いたい処理をここに書く
+
+                    // 画面の左側を離したら左のフリッパーが戻る
+                    if ((touch.position.x <= Screen.width / 2) && (touch.phase == TouchPhase.Ended && tag == "LeftFripperTag"))
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+
+                    // 画面の右側を離したら右のフリッパーが戻る
+                    if ((touch.position.x >= Screen.width / 2) && (touch.phase == TouchPhase.Ended && tag == "RightFripperTag"))
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                    break;
+            }
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/score.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    //ゲームオーバを表示するテキスト
+    private GameObject scoreText;
+
+    public int totalscore = 0; 　// 合計点数
+
+    void Start()
+    {
+      //シーン中のGameOverTextオブジェクトを取得
+      this.scoreText = GameObject.Find("scoreText");
+
+    }
+
+    //ボール接触時に点を獲得する
+    void OnCollisionEnter(Collision collision)
+    {
+        {
+
+            // タグ別に点数を獲得する
+            if (collision.gameObject.tag == "SmallStarTag")
+            {
+                totalscore += 10;
+            }
+
+            else if (collision.gameObject.tag == "LargeStarTag")
+            {
+                totalscore += 20;
+            }
+
+            else if (collision.gameObject.tag == "SmallCloudTag")
+            {
+                totalscore += 30;
+            }
+
+            else if (collision.gameObject.tag == "LargeCloudTag")
+            {
+                totalscore += 40;
+            }
+
+            Debug.Log(totalscore);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //scoreTextに合計点数を表示
+        this.scoreText.GetComponent<Text>().text = totalscore + "点";
+    }
+}
